4ec81a76b74c01bd420febe57ddc82575458503b
ThiagoCoser <thiago.coser@gmail.com>
2025-08-07 16:18:50 -0300

    Vasinho

---
+++ Assets/_Game/Scripts/Vasinho.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vasinho : MonoBehaviour
+{
+
+private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+
+            Destroy(gameObject);
+            
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
