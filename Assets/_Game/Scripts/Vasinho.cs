54dd2771f3c59c975b2d5b53ebedea8d465afb28
ThiagoCoser <thiago.coser@gmail.com>
2025-08-07 16:24:59 -0300

    Update Vasinho.cs

--- Assets/_Game/Scripts/Vasinho.cs
+++ Assets/_Game/Scripts/Vasinho.cs
@@ -9,23 +9,10 @@ private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ground"))
         {
-
-            Destroy(gameObject);
             
+            Destroy(gameObject);
+
         }
     }
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
