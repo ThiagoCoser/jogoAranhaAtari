2ba9d5e14fdbf42d14b52dfbeb45d19bcffbf9d5
ThiagoCoser <thiago.coser@gmail.com>
2025-08-07 15:11:57 -0300

    Vasinho

---
+++ Assets/Detector.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    public bool colidindo;
+    public bool podeDerrubar = true;
+
+
+    IEnumerator derrubaVaso()
+    {
+        //Instanciar o prefab do vasinho
+        Debug.Log("Derrubou");
+        podeDerrubar = false;
+        yield return new WaitForSeconds(2f);
+        podeDerrubar = true;
+              Debug.Log("Reset");
+
+
+}
+
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Derrubar") == true&&colidindo==true&&podeDerrubar==true)
+        {
+
+
+            StartCoroutine(derrubaVaso());
+
+
+        }
+
+
+}
+
+
+
+    // Detecta quando algo entra no trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colidindo = true;
+            Debug.Log(colidindo);
+        }
+    }
+
+    // Detecta enquanto o objeto está dentro do trigger
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colidindo = false;
+            Debug.Log(colidindo);
+        }
+    }
+
+}
+    
+
