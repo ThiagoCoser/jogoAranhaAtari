c0e1b1de26f889743ec605688c47bef61721dc80
ThiagoCoser <thiago.coser@gmail.com>
2025-08-13 16:45:59 -0300

    Stargame

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool iniciado = false;
+    public GameObject playerGame;
+    public GameObject textoCanvas;
+
+    private void iniciaJogo()
+    {
+        iniciado = true;
+        playerGame.GetComponent<Player>().enabled = true;
+        playerGame.GetComponent<BoxCollider>().enabled = true;
+        textoCanvas.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Derrubar") && iniciado == false)
+        {
+
+            iniciaJogo();
+
+
+        }
+
+
+    }
+}
