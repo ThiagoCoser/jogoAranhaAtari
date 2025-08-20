977f4578a0429646275ec7000fc48a92d9d1de57
ThiagoCoser <thiago.coser@gmail.com>
2025-08-20 14:26:05 -0300

    Pause

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -17,50 +16,47 @@ public class GameManager : MonoBehaviour
         playerGame.GetComponent<Player>().enabled = true;
         playerGame.GetComponent<BoxCollider>().enabled = true;
         textoCanvas.SetActive(false);
-
     }
 
-    private void pausaJogo()
+    private IEnumerator PausarCoroutine()
     {
+        pausado = true;
         Time.timeScale = 0;
+
+        Debug.Log("Jogo pausado");
+
+        yield return new WaitForSecondsRealtime(0.5f); // <- CORRIGIDO AQUI
+
+        // Espera até que o botão "Pausar" seja pressionado novamente
+        while (pausado==true)
+        {
+            if (Input.GetButtonDown("Pausar")){
+
+                Time.timeScale = 1;
+                pausado = false;
+                Debug.Log("Jogo despausado");
+
+                break;
+            }
+            yield return null; // espera o próximo frame
+        }
+
+        
+
+        
     }
 
-    private void despausaJogo()
-    {
-        Time.timeScale = 1;
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
-
-      //  Debug.Log(Time.timeScale);
-
-        if (Input.GetButtonDown("Derrubar") && iniciado == false)
+        if (Input.GetButtonDown("Derrubar") && !iniciado)
         {
-
             iniciaJogo();
-
-
         }
 
-        if (Input.GetButtonDown("Pausar") && pausado == false)
+        // Inicia a corrotina de pausa se ainda não estiver pausado
+        if (Input.GetButtonDown("Pausar") && !pausado)
         {
-
-            pausaJogo();
-
-
+            StartCoroutine(PausarCoroutine());
         }
-
-        if (Input.GetButtonDown("Pausar") && pausado == true)
-        {
-
-            despausaJogo();
-
-
-        }
-
-
     }
 }
